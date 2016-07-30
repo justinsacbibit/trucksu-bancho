d89810a1fedb946ed14920a64ce71507af2a9364
Justin Sacbibit <jsacbibit@gmail.com>
2016-07-29 21:57:30 -0400

    trigger build

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Game.Mixfile do
 
   def project do
     [app: :game,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
